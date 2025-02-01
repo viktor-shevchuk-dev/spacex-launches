0a2ebe83782b36a06b413b1b03948a7872141111
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-02-01 16:38:05 +0100

    revert strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
