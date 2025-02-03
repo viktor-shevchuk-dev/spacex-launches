75d3c591ddede7fabc4ffb2ed533365f6a7af0fe
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-02-03 14:03:26 +0100

    remove promise return from handlers types

--- src/types/handlers.type.ts
+++ src/types/handlers.type.ts
@@ -1,10 +1,10 @@
 export type ChangeLaunchCostHandler = (
   rocketId: string,
   field: { cost_per_launch: number }
-) => Promise<void>;
+) => void;
 
 export type ChangePayloadTypeHandler = (
   launchId: string,
   payloadId: string,
   field: { payload_type: string }
-) => Promise<void>;
+) => void;
